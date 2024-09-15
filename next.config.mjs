3e1280858c8b052f0c6a2e343e1aecd564d1735b
Janani <75440572+Jananisankar21419@users.noreply.github.com>
2024-09-15 22:17:15 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+  },
+  experimental: {
+    appDir: true,
+  },
+};
+
+export default nextConfig;
